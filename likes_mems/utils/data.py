69fff3612979e46c67283455975b5fb34afab7a9
Igor Saraev <director.kernelix@yandex.ru>
2023-02-18 21:20:34 +1000

    load_data v1.0

---
+++ likes_mems/utils/data.py
@@ -0,0 +1,7 @@
+TEST_DATA_USER = (
+    ('IosifBrodskiy', 'IosifBrodskiy', 'Iosif', 'Brodskiy', 'a@a.ru'),
+    ('NikolayGumilev', 'NikolayGumilev', 'Nikolay', 'Gumilev', 'b@b.ru'),
+    ('MihailLermontov', 'MihailLermontov', 'Mihail', 'Lermontov', 'c@c.ru'),
+    ('FedorDostoevskiy', 'FedorDostoevskiy', 'Fedor', 'Dostoevskiy', 'd@d.ru'),
+    ('LevTolstoy', 'LevTolstoy', 'Lev', 'Tolstoy', 'e@e.ru'),
+)
